ce5206ecac9c8de5cd7de8e15522135735f31cdd
Mordechai Meisels <mordechai112@gmail.com>
2022-11-30 10:32:56 -0500

    Redo home screen top bar, avoid cumulative layout shift

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -5,14 +5,13 @@ import s from './Banner.module.scss'
 export const Banner = () => {
     return <div className={s.banner}>
 
-        <Logo className={s.logo} />
-
-        <div className={s.menu}>
-            <Link href="/portfolio" className={s.menuItem}>Work</Link>
-            <Link href="/contact" className={s.menuItem}>Contact</Link>
+        <div className={s.topbar}>
+            <Logo className={s.logo} />
+            <div className={s.menu}>
+                <Link href="/portfolio" className={s.item}>Work</Link>
+                <Link href="/contact" className={s.item}>Contact</Link>
+            </div>
         </div>
-
-        <br />
 
         <p className={s.header}>
             A branding and web agency in Brooklyn.
