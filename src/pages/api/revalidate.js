b4bd678c0437750d55263cf6406d624b147d985a
Mordechai Meisels <mordechai112@gmail.com>
2022-12-08 13:34:40 -0500

    Update Next

--- src/pages/api/revalidate.js
+++ src/pages/api/revalidate.js
@@ -2,7 +2,6 @@ export default async function handler(req, res) {
     if (req.method !== 'POST')
         return res.status(405).send()
 
-    // TODO
     const {
         home,
         portfolio,
